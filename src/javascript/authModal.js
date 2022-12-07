028629520dc26116d4ec3a89daa10799abdbb010
Iuliia Sokol <avare779@gmail.com>
2022-12-07 17:40:48 +0200

    added btn for google sign in

--- src/javascript/authModal.js
+++ src/javascript/authModal.js
@@ -26,23 +26,32 @@ export function onSignInBtnClick(event) {
 
 <div class="lightbox-modal__form-wrapper">
 <form class="lightbox-modal__auth-form">
-
+<div class="auth-inputs-wrapper">
 <input type="email" id="auth-email" class="lightbox-modal__auth-input js-email-input">
 <label class="lightbox-modal__auth-label" for="auth-email">Email</label>
 
 <input type="password" id="auth-password" class="lightbox-modal__auth-input js-password-input">
 <label class="lightbox-modal__auth-label" for="auth-password">Password</label>
-
+</div>
 <div class="lightbox-modal__buttons lightbox-modal__auth-buttons">
 <button type="button" id="js-signInWithEmailBtn" class="lightbox-modal__button js-signInWithEmailBtn">Log in</button>
-<div class="auth-create-btn-wrapper">
+
+<div class="auth-google-btn-wrapper">
 <button type="button" id="js-createUserBtn" class="lightbox-modal__button">Create account</button>
-<div class="auth-create-notify">
+<div class="auth-google-notify">
 <p class="auth-notify">Newcomer? Create an account!</p>
 </div>
 </div>
+
 </div>
 
+<div class="auth-create-btn-wrapper">
+<button type="button" id="js-signInWithGoogleBtn" class="lightbox-modal__button js-signInWithGoogleBtn">
+<svg class="lightbox-modal__button-img" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" viewBox="0 0 48 48"><defs><path id="a" d="M44.5 20H24v8.5h11.8C34.7 33.9 30.1 37 24 37c-7.2 0-13-5.8-13-13s5.8-13 13-13c3.1 0 5.9 1.1 8.1 2.9l6.4-6.4C34.6 4.1 29.6 2 24 2 11.8 2 2 11.8 2 24s9.8 22 22 22c11 0 21-8 21-22 0-1.3-.2-2.7-.5-4z"/></defs><clipPath id="b"><use xlink:href="#a" overflow="visible"/></clipPath><path fill="#FBBC05" d="M0 37V11l17 13z" clip-path="url(#b)"/><path fill="#EA4335" d="m0 11 17 13 7-6.1L48 14V0H0z" clip-path="url(#b)"/><path fill="#34A853" d="m0 37 30-23 7.9 1L48 0v48H0z" clip-path="url(#b)"/><path fill="#4285F4" d="M48 48 17 24l-4-3 35-10z" clip-path="url(#b)"/></svg>
+</button>
+<div class="auth-create-notify">
+<p class="auth-notify">Log in with Google!</p>
+</div>
 </form>
 </div>
 
