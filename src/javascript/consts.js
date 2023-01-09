0fa97976a3df78022f8ea61ef375ccff68c0808f
Iuliia Sokol <avare779@gmail.com>
2023-01-09 08:13:24 +0200

    Update consts.js

--- src/javascript/consts.js
+++ src/javascript/consts.js
@@ -5,7 +5,16 @@ dotenv.config();
 export const API_KEY = process.env.API_KEY;
 
 //  Firebase configuration
-export const FIREBASE_CONFIG = JSON.parse(process.env.FIREBASE_CONFIG);
+
+export const FIREBASE_CONFIG = {
+  apiKey: process.env.apiKey,
+  authDomain: process.env.authDomain,
+  databaseURL: process.env.databaseURL,
+  projectId: process.env.projectId,
+  storageBucket: process.env.storageBucket,
+  messagingSenderId: process.env.messagingSenderId,
+  appId: process.env.appId,
+};
 
 console.log(FIREBASE_CONFIG);
 console.log(API_KEY);
